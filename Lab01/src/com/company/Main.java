91c9485b780ba279e052fabeb6955121b5f4186a
Rafal Szachowski <evkszach@gmail.com>
2022-03-12 21:07:04 +0100

    repo clean

---
+++ Lab01/src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // nowszy sposób
+        try {
+            byte[] data = Files.readAllBytes(Paths.get("E:\\JavaProjects\\Laby Backend\\lab_backEnd\\src\\file.txt"));
+            String text = new String(data, StandardCharsets.UTF_8);
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        File file = new File("E:\\JavaProjects\\Laby Backend\\lab_backEnd\\src\\file.txt");
+        try (
+                FileInputStream inputStream = new FileInputStream(file);
+                InputStreamReader fileReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        )
+        {
+            char[] buffer = new char[10];
+
+            while(true) {
+                int readBytes = fileReader.read(buffer, 0 , buffer.length);
+                if (readBytes == -1 ) {
+                    break;
+                }
+                System.out.println(buffer);
+
+            }
+            fileReader.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
