4d52504716caabe0e38659af41b4986d1d480773
Rafal Szachowski <evkszach@gmail.com>
2022-05-17 18:51:52 +0200

    Entities: Company, Location

---
+++ projekt/WarehouseManager/WarehouseManager/Entites/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace WarehouseManager.Entites
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public virtual List<Location> Locations { get; set; }
+    }
+}
---
+++ projekt/WarehouseManager/WarehouseManager/Entites/Location.cs
@@ -0,0 +1,14 @@
+﻿namespace WarehouseManager.Entites
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string LocationName { get; set; }
+        public string Description { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string ZipCode{ get; set; }
+        public int CompanyId { get; set; }
+        public virtual Company Company { get; set; }
+    }
+}
