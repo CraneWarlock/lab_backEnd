16898009ec2b21466530bf4b6bc38860e8396205
Rafal Szachowski <evkszach@gmail.com>
2022-05-17 21:40:02 +0200

    add db service

--- projekt/WarehouseManager/WarehouseManager/Program.cs
+++ projekt/WarehouseManager/WarehouseManager/Program.cs
@@ -1,8 +1,11 @@
+using WarehouseManager.Entites;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<WarehauseManagerDbContext>();
 
 var app = builder.Build();
 
