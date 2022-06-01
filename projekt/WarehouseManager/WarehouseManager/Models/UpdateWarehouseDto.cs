26531cdee3d00ca1f3994bc5247d07cdb3cdbded
Rafal Szachowski <evkszach@gmail.com>
2022-06-01 23:52:01 +0200

    Warehouse service created

--- projekt/WarehouseManager/WarehouseManager/Models/UpdateWarehouseDto.cs
+++ projekt/WarehouseManager/WarehouseManager/Models/UpdateWarehouseDto.cs
@@ -4,7 +4,8 @@
     {
         public string WarehouseName { get; set; }
         public string Description { get; set; }
-        public decimal CurrentCapacity { get; set; }
+
+       // public decimal CurrentCapacity { get; set; }
         public decimal MaximumCapacity { get; set; }
     }
 }
---
+++ projekt/WarehouseManager/WarehouseManager/Services/WarehouseService.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using WarehouseManager.Entites;
+using WarehouseManager.Models;
+
+namespace WarehouseManager.Services
+{
+    public class WarehouseService
+    {
+        private readonly WarehauseManagerDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public WarehouseService(WarehauseManagerDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public int Create(int locationId, CreateWarehouseDto dto)
+        {
+            var location = GetLocationById(locationId);
+            var warehouseEntity = _mapper.Map<Warehouse>(dto);
+            warehouseEntity.LocationId = locationId;
+
+            //TODO: exception here
+            if (dto.MaximumCapacity <= 0) return 0;
+            if (dto.CurrentCapacity != 0) return 0;
+
+            _dbContext.Warehouses.Add(warehouseEntity);
+            _dbContext.SaveChanges();
+            return warehouseEntity.Id;
+        }
+
+        public bool Update(int locationId, int warehouseId, UpdateWarehouseDto dto)
+        {
+            var location = GetLocationById(locationId);
+            var warehouse = _dbContext
+                .Warehouses
+                .FirstOrDefault(r => r.Id == locationId);
+
+            if (warehouse is null) return false;
+            if (dto.MaximumCapacity <= 0) return false;
+
+            warehouse.WarehouseName = dto.WarehouseName;
+            warehouse.Description = dto.Description;
+            warehouse.MaximumCapacity = dto.MaximumCapacity;
+            _dbContext.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(int locationId, int warehouseId)
+        {
+            var location = GetLocationById(locationId);
+            if (location is null) return false;
+            var warehouse = _dbContext
+                .Warehouses
+                .FirstOrDefault(r => r.Id == warehouseId);
+            if (warehouse is null) return false;
+
+            _dbContext.Warehouses.Remove(warehouse);
+            _dbContext.SaveChanges();
+            return true;
+        }
+
+        public WarehouseDto GetById(int locationId, int warehouseId)
+        {
+            var location = GetLocationById(locationId);
+            var warehouse = _dbContext
+                .Warehouses
+                .FirstOrDefault(r => r.Id == warehouseId);
+            if (warehouse is null || warehouse.LocationId != locationId) return null;
+
+            var warehouseDto = _mapper.Map<WarehouseDto>(warehouse);
+            return warehouseDto;
+        }
+
+        public List<WarehouseDto> GetAll(int locationId)
+        {
+            var location = GetLocationById(locationId);
+            var warehouseDtos = _mapper.Map<List<WarehouseDto>>(location.Warehouses);
+            return warehouseDtos;
+        }
+
+        private Location GetLocationById(int locationId)
+        {
+            var location = _dbContext
+                .Locations
+                .Include(r => r.Warehouses)
+                .FirstOrDefault(r => r.Id == locationId);
+            if (location == null) return null;
+            return location;
+        }
+
+    }
+}
